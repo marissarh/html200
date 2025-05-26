fa6c07db8946dd79b72ff1c4428d01b2aaba47c1
Marissa <102936996+marissarh@users.noreply.github.com>
2025-05-25 22:39:45 -0700

    add switch function

--- lesson06/challenge1.js
+++ lesson06/challenge1.js
@@ -1,2 +1,30 @@
-let x =4
-console.log(x);
+let user = prompt('Choose an Option');
+let withdraw = prompt('Withdraw Amount');
+let deposit = prompt('Deposit Amount');
+const amount = x;
+
+function userOption(option){
+    switch(option){
+        case 'Q':{
+            return user;
+            break;
+        }
+        case 'W':{
+            return withdraw [x];
+            break;
+            }
+        case 'D':{
+            return deposit [x];
+            break;
+        }
+        case 'B':{
+            return balance; 
+            break;
+        }
+        default:{
+            return user;
+        }
+        }
+    }
+
+console.log(userOption(amount));
