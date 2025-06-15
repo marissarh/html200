c204b4d9f744526e4b9e1e7ef74df2f1476ad39c
Marissa <102936996+marissarh@users.noreply.github.com>
2025-06-14 22:23:31 -0700

    add updated files

--- lesson09/challenge1/gulpfile.js
+++ lesson09/challenge1/gulpfile.js
@@ -1,39 +1,54 @@
 const {
     src,
     dest,
+    parallel,
     series,
     watch,
-} = require('gulp');
+    task
+} = require('gulp')
+const browserSync = require('browser-sync');
+const server = browserSync.create();
 const sass = require('gulp-sass')(require('sass'));
-const prefix = require('gulp-autoprefixer');
-const minify = require('gulp-clean-css');
-const rename = require('gulp-rename');
 
-
-function compilescss(){
-    return src('./src/scss/**/*.scss')
-    .pipe(sass())
-    .pipe(prefix())
-    .pipe(minify())
-    .pipe(rename(function(path) {
-        return{
-            dirname: path.dirname + "",
-            basename: path.basename + ".min",
-            extname: ".css"
-        };
-    }))
-    .pipe(dest('.dist.css'))
+// Directories to watch.
+// If watch & reload isn't working as expected, check that files you want watched can be found in these paths.
+const paths = {
+    scss: {src: './css/*.scss', dest: './css'},
+    data: {src: './data/', dest: './data/'},
+    js: {src: './*.js', dest: '.'},
+    html: {src: './*.html', dest: '.'}
 };
 
 
+// Compile SCSS into CSS
+task('sass', function() {
+    return src(paths.scss.src)
+        .pipe(sass({
+            includePaths: [paths.scss],
+            outputStyle: 'compressed'
+        }).on('error', function(err) {
+            console.log(err.message);
+            this.emit('end');
+        }))
+        .pipe(dest(paths.scss.dest))
+        .pipe(browserSync.stream());
+});
+
+// BrowserSync configuration
+task('browserSync', function() {
+    browserSync.init({
+        server: './',
+        notify: false
+    });
+})
 
 // Watch for changes in scss, html, and js files, reloading page in browser when change is found.
-function watchTask(){
-    watch('.src/scss/**/*scss', compilescss);
-}
+task('watchFiles', function() {
+    watch(paths.scss.src, parallel('sass')).on('change', browserSync.reload);
+    watch(paths.html.src).on('change', browserSync.reload);
+    watch(paths.js.src).on('change', browserSync.reload);
+});
 
-exports.default = series(
-    compilescss,
-    watchTask
+const watching = parallel('watchFiles', 'browserSync')
 
-)
+exports.default = series(watching)
